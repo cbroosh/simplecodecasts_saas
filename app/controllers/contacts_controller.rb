83629a18b2a862b74826ffc355078b24f1276bbb
chris brucher <c.p.brucher@gmail.com>
2015-06-30 01:01:02 +0000

    Added form validation, messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,25 @@
 class ContactsController < ApplicationController
-  def new
-    @contact = Contact.new
-  end
+
+    def new
+      @contact = Contact.new
+    end
+
+   def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path
+    else
+      flash[:danger] = "Error occured."
+      redirect_to new_contact_path
+    end
+   end
   
-  def create
-  end
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
+ 
 end
 
   
