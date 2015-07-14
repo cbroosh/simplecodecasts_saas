7b779225a971b82e11dafbee222f525f09fba957
Chris Brucher <c.p.brucher@gmail.com>
2015-07-14 01:48:27 +0000

    Corrected merge error

--- config/environment.rb
+++ config/environment.rb
@@ -5,21 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-<<<<<<< HEAD
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'http://shielded-oasis-3657.herokuapp.com/',
-  :enable_startstls_auto => true
-=======
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],  ## Refer to key SENDGRID_USERNAME
-  :password       => ENV['SENDGRID_PASSWORD'],  ## Refer to key SENDGRID_PASSWORD
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
->>>>>>> development
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :authentication => :plain,
+    :user_name => ENV['SENDGRID_USERNAME'],  ## Refer to key SENDGRID_USERNAME
+    :password => ENV['SENDGRID_PASSWORD'],  ## Refer to key SENDGRID_PASSWORD
+    :domain => 'heroku.com',
+    :enable_starttls_auto => true
 }
