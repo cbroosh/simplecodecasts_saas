ac094ea54b47f1ca4aa3e5f7b8d7bd987dbdcb3d
Chris Brucher <c.p.brucher@gmail.com>
2015-07-16 01:18:58 +0000

    Unlocked contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,7 @@ class ContactsController < ApplicationController
         email = params[:contact][:email]
         body = params[:contact][:comments]
         
-        # ContactMailer.contact_email(name, email, body).deliver
+        ContactMailer.contact_email(name, email, body).deliver
         redirect_to new_contact_path
       else
         flash[:danger] = 'Error occured, message has not been sent.'
