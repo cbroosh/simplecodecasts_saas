57198f95915e9766338a9bc75bcb224a7a555477
Chris Brucher <c.p.brucher@gmail.com>
2015-07-21 22:27:23 +0000

    Reorganized pages for routes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,7 @@ class ProfilesController < ApplicationController
      def new
         # form for each user to fill out information regarding their own profile
         @user = User.find( params[:user_id] )
+        @variable = params[:hello]
         @profile = @user.build_profile
      end
 end
