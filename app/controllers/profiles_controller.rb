05d2883087691b03845da5e7d452d074a84bf30d
Chris Brucher <c.p.brucher@gmail.com>
2015-07-24 15:19:34 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,19 @@ class ProfilesController < ApplicationController
      def new
         # form for each user to fill out information regarding their own profile
         @user = User.find( params[:user_id] )
-        @variable = params[:hello]
         @profile = @user.build_profile
+     end
+     
+     def create
+        @user = User.find( params[:user_id] )
+        @profile = @user.build_profile(profile_params)
+        
+     end
+     
+     
+     private
+     # white listing variables from the form entry
+     def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description) 
      end
 end
